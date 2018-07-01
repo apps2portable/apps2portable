47eea6eb8f5bd134aee377764868166dfa49f94c
apps2portable <33720985+apps2portable@users.noreply.github.com>
2018-07-02 02:20:45 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*10)+ 1);
+var x=Math.floor((Math.random()*9)+ 1);
 var xxx=null;
 if(x=="1"){xxx="how-to-prevent-acne-in-humid-weather.html"}
 if(x=="2"){xxx="natural-ways-you-can-prevent-unwanted-hair-fall.html"}
@@ -30,7 +30,7 @@ if(x=="6"){xxx="how-chemical-peels-remove-acne-scars.html"}
 if(x=="7"){xxx="beating-calories-with-wise-dieting.html"}
 if(x=="8"){xxx="vitamin-c-for-healthier-and-longer-life.html"}
 if(x=="9"){xxx="how-to-safeguard-your-immune-system.html"}
-if(x=="10"){xxx="castor-oil-and-its-benefits.html"}a_to_ve[i].href="https://appsforhealth.net/2017/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="https://appsforhealth.net/2017/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
